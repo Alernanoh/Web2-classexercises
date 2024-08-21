7e4127102f0e21d740c28ee577cd66a451babc09
Alernanoh <alhernano@gmail.com>
2024-08-21 07:31:48 -0500

    adding tables

--- src/app/pages/pokemon-list/pokemon-list.component.ts
+++ src/app/pages/pokemon-list/pokemon-list.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { Pokemon } from '../../utils/pokemon';
+import * as pokemonData from '../../../../public/json/pokemonData.json';
+import { CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-pokemon-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pokemon-list.component.html',
   styleUrl: './pokemon-list.component.css'
 })
 export class PokemonListComponent {
 
+  pokemons: Pokemon[] = (pokemonData as any).default;
 }
